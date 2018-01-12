af43cbe5ba061f2322bf10cb0213ff5359cb6b11
Tom Bianchi <atbianchi@gmail.com>
2018-01-11 22:34:30 -0500

    console.log
    changed alerts to console.logs

--- script.js
+++ script.js
@@ -5,45 +5,45 @@ function test() {
 }
 
 function ex1() {
-  number = window.prompt('Enter a number!');
+  number = prompt('Enter a number!');
   var output = 0;
   for (i = 0; i <= number; i++) {
     output += i;
   };
-  alert(output);
+  console.log(output);
 }
 
 
 function ex2() {
   var output = "";
   var play = true;
-  ans1 = window.prompt('Do you want to play?').toLowerCase();
+  ans1 = prompt('Do you want to play?').toLowerCase();
   if (ans1 === "yes") {
     do {
-      word = window.prompt('Enter a word.');
+      word = prompt('Enter a word.');
       output = output + " " + word;
-      ans2 = window.prompt('Do you want to play again?');
+      ans2 = prompt('Do you want to play again?');
       if (ans2 === "no") {
         play = false;
       }
     }
     while (play === true);
-    alert(output);
+    console.log(output);
   }
 }
 
 
 function ex3() {
   var repeat = true;
-  name = window.prompt('What is your name?');
-  print = window.prompt('Would you like to print your name?');
+  name = prompt('What is your name?');
+  print = prompt('Would you like to print your name?');
   if (print.toLowerCase() == "yes") {
     output = "Hello. My name is " + name;
     var i = 0;
     while (repeat == true) {
       sentence = output + "!".repeat(i);
-      alert(sentence);
-      again = window.prompt("Would you like to print this again?");
+      console.log(sentence);
+      again = prompt("Would you like to print this again?");
       if (again != "yes") {
         repeat = false;
       }
@@ -53,14 +53,14 @@ function ex3() {
 }
 
 function ex4() {
-  day = window.prompt('What time of day is it?').toLowerCase();
+  day = prompt('What time of day is it?').toLowerCase();
   if (day == "morning") {
-    alert("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
+    console.log("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
   } else if (day == "noon") {
-    alert('Since it is noon, you should be eating lunch. We suggest a salad');
+    console.log('Since it is noon, you should be eating lunch. We suggest a salad');
   } else if (day == 'evening') {
-    alert('Since it is evening, you should be eating dinner. We suggest chicken and rice.');
+    console.log('Since it is evening, you should be eating dinner. We suggest chicken and rice.');
   } else {
-    alert("Please pick only morning, noon, or evening");
+    console.log("Please pick only morning, noon, or evening");
   }
 }
